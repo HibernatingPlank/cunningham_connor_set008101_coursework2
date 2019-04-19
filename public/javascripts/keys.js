1cc912fbf3ecedf8c76888db943d186253cd1afe
HibernatingPlank <HibernatingPlank@gmail.com>
2019-04-19 13:12:08 +0100

    Add files via upload

---
+++ public/javascripts/keys.js
@@ -0,0 +1,5 @@
+dbPassword = 'mongodb+srv://HibernatingPlank:'+ encodeURIComponent('zopreSk2evqnwblt19@') + '@assessment2-sfb7x.mongodb.net/test?retryWrites=true';
+
+module.exports = {
+    mongoURI: dbPassword
+};
